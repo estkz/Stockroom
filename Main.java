5842097900cd04e746a6e6c65fbf5c7f44ccd691
estkz <3stetik@gmail.com>
2023-05-16 14:20:03 +0200

    init

--- Main.java
+++ Main.java
@@ -3,7 +3,5 @@ public class Main {
     public static void main(String[] args) {
         Voorraad.getDatabasePlekken();
         new Frame();
-        System.out.println("Bing Chilling");
     }
 }
-// Henk de penk
