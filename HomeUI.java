6f8b60d8af217ac118f5d2d6bb7b18efa07b65be
Papflap <tommooitip@gmail.com>
2023-05-25 12:15:30 +0200

    arm naar HomeUI.java

--- HomeUI.java
+++ HomeUI.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Objects;
 
-public class Arm extends JPanel implements ActionListener {
+public class HomeUI extends JPanel implements ActionListener {
     Database db = new Database();
     JFrame parentFrame;
 
@@ -24,7 +24,7 @@ public class Arm extends JPanel implements ActionListener {
     JButton voorraadVerwijderen = new JButton("Voorraad verwijderen");
 
 
-    Arm(JFrame parentFrame){
+    HomeUI(JFrame parentFrame){
         this.parentFrame = parentFrame;
 
         setBounds(420,0,200,400);
