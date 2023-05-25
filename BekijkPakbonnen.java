143c61b77bdc215ce6f94bc78f4a1d8e571ea770
Kroketbroodje <116641450+Kroketbroodje@users.noreply.github.com>
2023-05-25 13:56:54 +0200

    Basis pakbon weergave

--- BekijkPakbonnen.java
+++ BekijkPakbonnen.java
@@ -2,7 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class BekijkPakbonnen extends JDialog {
+    Database db = new Database();
+    JButton haalPakbon = new JButton("Ophalen pakbon van order");
+    DefaultListModel<String> listModel = new DefaultListModel<>();
+    JList<String> pakbonList = new JList<>(listModel);
     JFrame parentFrame;
+
     BekijkPakbonnen(JFrame frame, boolean m){
         super(frame, m);
         this.parentFrame = frame;
@@ -12,6 +17,15 @@ public class BekijkPakbonnen extends JDialog {
         setLayout(new FlowLayout());
         setResizable(false);
 
+
+
+        add(new JLabel("Pakbon halen: "));
+        add(pakbonList);
+        add(haalPakbon);
+
+        pakbonList.setPreferredSize(new Dimension(550,400));
+        pakbonList.setLayout(new GridLayout(5,2));
+        pakbonList.setBorder(BorderFactory.createLineBorder(Color.black));
 
         setLocationRelativeTo(null);
         setVisible(true);
