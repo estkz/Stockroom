fdb8daab675dbd1ba26dfd573660131587e295bf
estkz <3stetik@gmail.com>
2023-05-26 14:31:27 +0200

    init

--- Binpacking.java
+++ Binpacking.java
@@ -3,29 +3,37 @@ import java.util.List;
 
 public class Binpacking {
     public static void main(String[] args) {
-        // Example of items sorted in decreasing order (by weight in kg)
+        // Example Items - Displayed in Kilograms
         int[] items = {90, 80, 70, 60, 50, 40, 30, 20, 10};
         int binCapacity = 100;
-        int binCount = bestFitDecreasing(items, binCapacity);
-        System.out.println("Number of bins required: " + binCount);
+        List<List<Integer>> bins = bestFitDecreasing(items, binCapacity);
+        System.out.println("Number of bins required: " + bins.size());
+        for (int i = 0; i < bins.size(); i++) {
+            System.out.println("Bin " + (i + 1) + ": " + bins.get(i));
+        }
     }
 
-    public static int bestFitDecreasing(int[] items, int binCapacity) {
-        List<Integer> bins = new ArrayList<>();
+    public static List<List<Integer>> bestFitDecreasing(int[] items, int binCapacity) {
+        List<List<Integer>> bins = new ArrayList<>();
+        List<Integer> binRemainingCapacity = new ArrayList<>();
         for (int item : items) {
             int j = 0;
-            // Find best bin where the item can fit in
-            for (j = 0; j < bins.size(); j++) {
-                if (bins.get(j) >= item) {
-                    bins.set(j, bins.get(j) - item);
+            // Find best bin where the item(s) fit
+            for (j = 0; j < binRemainingCapacity.size(); j++) {
+                if (binRemainingCapacity.get(j) >= item) {
+                    binRemainingCapacity.set(j, binRemainingCapacity.get(j) - item);
+                    bins.get(j).add(item);
                     break;
                 }
             }
-            // If no bin could fit the item, it will create a new bin.
-            if (j == bins.size()) {
-                bins.add(binCapacity - item);
+            // If no bin has the ability to store an item, create a new bin
+            if (j == binRemainingCapacity.size()) {
+                binRemainingCapacity.add(binCapacity - item);
+                List<Integer> newBin = new ArrayList<>();
+                newBin.add(item);
+                bins.add(newBin);
             }
         }
-        return bins.size();
+        return bins;
     }
 }
