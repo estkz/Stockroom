4fa02fea9f5bc8e8a4a58cc81bfccf23e45fcedc
estkz <3stetik@gmail.com>
2023-05-26 13:25:21 +0200

    init

--- Binpacking.java
+++ Binpacking.java
@@ -1,16 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Binpacking {
-
-    // Best-Fit Algorithm | Decreasing Size (Big -> Small)
-
-    // Vars
-    double weight; // Product weight in kg
-    double capacity = 50; // Capacity in kg
-    int bin[] = { 1, 2, 3, 4, 5 }; // Amount of bins available
-
-    Binpacking() {
-
-
-
+    public static void main(String[] args) {
+        // Example of items sorted in decreasing order (by weight in kg)
+        int[] items = {90, 80, 70, 60, 50, 40, 30, 20, 10};
+        int binCapacity = 100;
+        int binCount = bestFitDecreasing(items, binCapacity);
+        System.out.println("Number of bins required: " + binCount);
     }
 
+    public static int bestFitDecreasing(int[] items, int binCapacity) {
+        List<Integer> bins = new ArrayList<>();
+        for (int item : items) {
+            int j = 0;
+            // Find best bin where the item can fit in
+            for (j = 0; j < bins.size(); j++) {
+                if (bins.get(j) >= item) {
+                    bins.set(j, bins.get(j) - item);
+                    break;
+                }
+            }
+            // If no bin could fit the item, it will create a new bin.
+            if (j == bins.size()) {
+                bins.add(binCapacity - item);
+            }
+        }
+        return bins.size();
+    }
 }
