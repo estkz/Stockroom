6268dfbd5dae4e8d11e9f6cd918b0d7cd73bd496
estkz <3stetik@gmail.com>
2023-05-26 15:43:14 +0200

    init

--- BekijkPakbonnen.java
+++ BekijkPakbonnen.java
@@ -11,46 +11,48 @@ public class BekijkPakbonnen extends JDialog {
     String packingSlipTitle = "Pakbon ";
     String clientName = "Tom Prachtig ";
 
+    int orderID = 1;
+
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
     LocalDateTime current = LocalDateTime.now();
     GridBagConstraints gbc = new GridBagConstraints();
 
 
+    // Paint Bekijk Pakbonnen Dialog
     BekijkPakbonnen(JFrame frame, boolean m){
         super(frame, m);
         this.parentFrame = frame;
 
+        // Dialog Properties
         setSize(new Dimension(600,600));
         setTitle("Bekijk pakbonnen");
-
-        //setLayout(new FlowLayout(FlowLayout.LEFT));
-        //setLayout(new GridLayout(0, 1, 0, 20));
-        //setLayout(new GridLayout(20, 1));
-        //setLayout(new GridBagLayout());
         setResizable(false);
 
-
-
-        JLabel packingSlipNumber = new JLabel("420");
-        JLabel orderID = new JLabel("1");
-        JLabel klantNaam = new JLabel("Naam: " + clientName);
-        JLabel date = new JLabel("Datum: " + dtf.format(current));
+        // Components
+        JLabel lPpackingSlipNumber = new JLabel("420");
+        JLabel lOrderID = new JLabel("Order ID: " + orderID);
+        JLabel lKlantNaam = new JLabel("Naam: " + clientName);
+        JLabel lDate = new JLabel("Datum: " + dtf.format(current));
         JLabel titlePackingSlip = new JLabel(packingSlipTitle);
+
+        // Top Panel | Pakbon Title Alignment
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(titlePackingSlip, BorderLayout.CENTER);
-
-        JPanel mainPanel = new JPanel(new BorderLayout());
-
         JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         topPanel.add(titlePackingSlip);
+
+        // Main Panel
+        JPanel mainPanel = new JPanel(new BorderLayout());
         mainPanel.add(topPanel, BorderLayout.NORTH);
-
-        JPanel rightPanel = new JPanel(new GridLayout(20, 1));
-        rightPanel.add(klantNaam);
-        rightPanel.add(date);
-
-        mainPanel.add(rightPanel, BorderLayout.WEST);
         add(mainPanel);
+
+        // Left Panel | Client/Order Information
+        JPanel leftPanel = new JPanel(new GridLayout(20, 1));
+        leftPanel.add(lOrderID);
+        leftPanel.add(lKlantNaam);
+        leftPanel.add(lDate);
+        mainPanel.add(leftPanel, BorderLayout.WEST);
+
 
 
 
